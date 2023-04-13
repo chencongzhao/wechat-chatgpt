42b73cdf0b8ca70ba5816adc86b3ed04980e9523
chencongzhao <128721714+chencongzhao@users.noreply.github.com>
2023-04-14 01:00:50 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,6 @@ export const config: IConfig = {
   chatTriggerRule: process.env.CHAT_TRIGGER_RULE || "@猫猫",
   disableGroupMessage: process.env.DISABLE_GROUP_MESSAGE === "false",
   temperature: process.env.TEMPERATURE ? parseFloat(process.env.TEMPERATURE) : 1.3,
-  blockWords: process.env.BLOCK_WORDS?.split(",") || ["AI","人工智能"，"chatgpt"，"openai"，"规范"，"抱歉"，"我不能"],
-  chatgptBlockWords: process.env.CHATGPT_BLOCK_WORDS?.split(",") || ["AI","人工智能"，"chatgpt"，"openai"，"规范"，"抱歉"，"我不能"],
+  blockWords: process.env.BLOCK_WORDS?.split(",") || ["AI","人工智能","chatgpt","openai","规范","抱歉","我不能"],
+  chatgptBlockWords: process.env.CHATGPT_BLOCK_WORDS?.split(",") || ["AI","人工智能","chatgpt","openai","规范","抱歉","我不能"],
 };
